117fad3ce94aa13016d6842b6ae854e74b5a4983
mlutfipratama <muhamadlutfipratama6@gmail.com>
2025-07-01 10:30:29 +0700

    adding custom favicon

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Cerita Rakyat Sunda</title>
+    <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 
