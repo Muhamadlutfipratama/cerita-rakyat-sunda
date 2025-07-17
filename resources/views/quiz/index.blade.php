f87cbff5772c68b44b5473ed8a71bbfe6e726c61
mlutfipratama <muhamadlutfipratama6@gmail.com>
2025-07-17 14:19:55 +0700

    change kuis to quiz

--- resources/views/quiz/index.blade.php
+++ resources/views/quiz/index.blade.php
@@ -2,7 +2,7 @@
 @extends('layout')
 @section('content')
     <div class="container mt-4 h-80">
-        <h2 class="mb-4">Daftar Kuis</h2>
+        <h2 class="mb-4">Daftar Quiz</h2>
         <div class="row">
             @forelse ($quizzes as $quiz)
                 <div class="col-md-6 mb-3">
@@ -10,7 +10,7 @@
                         <div class="card-body">
                             <h5>{{ $quiz->title }}</h5>
                             <p>{{ $quiz->description }}</p>
-                            <a href="{{ route('quiz.show', $quiz) }}" class="btn btn-primary">Mulai Kuis</a>
+                            <a href="{{ route('quiz.show', $quiz) }}" class="btn btn-primary">Mulai Quiz</a>
                         </div>
                     </div>
                 </div>
